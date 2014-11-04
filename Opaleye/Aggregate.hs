ed762d3f9444234470ea1166e2c9315ed0db348c
Tom Ellis <tom-2013@purelyagile.com>
2014-11-04 17:09:52 +0000

    Move internals to Opaleye.Internal.Aggregator

--- Opaleye/Aggregate.hs
+++ Opaleye/Aggregate.hs
@@ -1,81 +1,21 @@
 module Opaleye.Aggregate where
 
-import qualified Opaleye.Internal.PackMap as PM
-import qualified Opaleye.Internal.Tag as T
+import qualified Opaleye.Internal.Aggregate as A
+import           Opaleye.Internal.Aggregate (Aggregator)
 import           Opaleye.QueryArr (Query)
 import qualified Opaleye.QueryArr as Q
 import qualified Opaleye.Column as C
 
 import qualified Database.HaskellDB.PrimQuery as PQ
 
-import qualified Control.Monad.Trans.State as S
-import           Control.Applicative (Applicative, pure, (<*>))
-
-import qualified Data.Profunctor as P
-import qualified Data.Profunctor.Product as PP
-
-newtype Aggregator a b = Aggregator
-                         (PM.PackMap (PQ.PrimExpr, Maybe PQ.AggrOp) PQ.PrimExpr
-                                     a b)
-
 groupBy :: Aggregator (C.Column a) (C.Column a)
-groupBy = makeAggr' Nothing
+groupBy = A.makeAggr' Nothing
 
 sum :: Aggregator (C.Column a) (C.Column a)
-sum = makeAggr PQ.AggrSum
+sum = A.makeAggr PQ.AggrSum
 
 count :: Aggregator (C.Column a) (C.Column a)
-count = makeAggr PQ.AggrCount
-
-makeAggr' :: Maybe PQ.AggrOp -> Aggregator (C.Column a) (C.Column a)
-makeAggr' m = Aggregator (PM.PackMap
-                          (\f (C.Column e) -> fmap C.Column (f (e, m))))
-
-makeAggr :: PQ.AggrOp -> Aggregator (C.Column a) (C.Column a)
-makeAggr = makeAggr' . Just
-
-runAggregator :: Applicative f => Aggregator a b
-              -> ((PQ.PrimExpr, Maybe PQ.AggrOp) -> f PQ.PrimExpr) -> a -> f b
-runAggregator (Aggregator a) = PM.packmap a
+count = A.makeAggr PQ.AggrCount
 
 aggregate :: Aggregator a b -> Query a -> Query b
-aggregate agg q = Q.simpleQueryArr (aggregateU agg . Q.runSimpleQueryArr q)
-
--- FIXME: duplication with distinctU
--- FIXME: probably need to use the tag
-aggregateU :: Aggregator a b
-           -> (a, PQ.PrimQuery, T.Tag) -> (b, PQ.PrimQuery, T.Tag)
-aggregateU agg (c0, primQ, t0) = (c1, primQ', t0)
-  where f :: (PQ.PrimExpr, Maybe PQ.AggrOp)
-          -> S.State ([(String, PQ.PrimExpr)], Int) PQ.PrimExpr
-        f (pe, maggrop) = do
-          (projPEs, i) <- S.get
-          let s = "result" ++ show i
-          let aggrpe = case maggrop of
-                Nothing -> id
-                Just aggrop -> PQ.AggrExpr aggrop
-          S.put (projPEs ++ [(s, aggrpe pe)], i+1)
-          return (PQ.AttrExpr s)
-
-        (c1, (projPEs', _)) =
-          S.runState (runAggregator agg f c0) ([], 0)
-
-        primQ' = PQ.Project projPEs' primQ
-
--- { Boilerplate instances
-
-instance Functor (Aggregator a) where
-  fmap f (Aggregator g) = Aggregator (fmap f g)
-
-instance Applicative (Aggregator a) where
-  pure = Aggregator . pure
-  Aggregator f <*> Aggregator x = Aggregator (f <*> x)
-
-instance P.Profunctor Aggregator where
-  dimap f g (Aggregator q) = Aggregator (P.dimap f g q)
-
-instance PP.ProductProfunctor Aggregator where
-  empty = PP.defaultEmpty
-  (***!) = PP.defaultProfunctorProduct
-
--- }
+aggregate agg q = Q.simpleQueryArr (A.aggregateU agg . Q.runSimpleQueryArr q)
---
+++ Opaleye/Internal/Aggregate.hs
@@ -0,0 +1,67 @@
+module Opaleye.Internal.Aggregate where
+
+import           Control.Applicative (Applicative, pure, (<*>))
+import qualified Control.Monad.Trans.State as S
+
+import qualified Data.Profunctor as P
+import qualified Data.Profunctor.Product as PP
+
+import qualified Opaleye.Internal.PackMap as PM
+import qualified Opaleye.Internal.Tag as T
+import qualified Opaleye.Column as C
+
+import qualified Database.HaskellDB.PrimQuery as PQ
+
+newtype Aggregator a b = Aggregator
+                         (PM.PackMap (PQ.PrimExpr, Maybe PQ.AggrOp) PQ.PrimExpr
+                                     a b)
+
+makeAggr' :: Maybe PQ.AggrOp -> Aggregator (C.Column a) (C.Column a)
+makeAggr' m = Aggregator (PM.PackMap
+                          (\f (C.Column e) -> fmap C.Column (f (e, m))))
+
+makeAggr :: PQ.AggrOp -> Aggregator (C.Column a) (C.Column a)
+makeAggr = makeAggr' . Just
+
+runAggregator :: Applicative f => Aggregator a b
+              -> ((PQ.PrimExpr, Maybe PQ.AggrOp) -> f PQ.PrimExpr) -> a -> f b
+runAggregator (Aggregator a) = PM.packmap a
+
+-- FIXME: duplication with distinctU
+-- FIXME: probably need to use the tag
+aggregateU :: Aggregator a b
+           -> (a, PQ.PrimQuery, T.Tag) -> (b, PQ.PrimQuery, T.Tag)
+aggregateU agg (c0, primQ, t0) = (c1, primQ', t0)
+  where f :: (PQ.PrimExpr, Maybe PQ.AggrOp)
+          -> S.State ([(String, PQ.PrimExpr)], Int) PQ.PrimExpr
+        f (pe, maggrop) = do
+          (projPEs, i) <- S.get
+          let s = "result" ++ show i
+          let aggrpe = case maggrop of
+                Nothing -> id
+                Just aggrop -> PQ.AggrExpr aggrop
+          S.put (projPEs ++ [(s, aggrpe pe)], i+1)
+          return (PQ.AttrExpr s)
+
+        (c1, (projPEs', _)) =
+          S.runState (runAggregator agg f c0) ([], 0)
+
+        primQ' = PQ.Project projPEs' primQ
+
+-- { Boilerplate instances
+
+instance Functor (Aggregator a) where
+  fmap f (Aggregator g) = Aggregator (fmap f g)
+
+instance Applicative (Aggregator a) where
+  pure = Aggregator . pure
+  Aggregator f <*> Aggregator x = Aggregator (f <*> x)
+
+instance P.Profunctor Aggregator where
+  dimap f g (Aggregator q) = Aggregator (P.dimap f g q)
+
+instance PP.ProductProfunctor Aggregator where
+  empty = PP.defaultEmpty
+  (***!) = PP.defaultProfunctorProduct
+
+-- }
