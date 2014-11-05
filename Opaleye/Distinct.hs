c3d98160242ec597cb1878c5ecf15e88e0028f33
Tom Ellis <tom-2013@purelyagile.com>
2014-11-05 19:07:54 +0000

    Move Distinct.distinctU to Internal.Distinct

--- Opaleye/Distinct.hs
+++ Opaleye/Distinct.hs
@@ -4,6 +4,7 @@ module Opaleye.Distinct where
 
 import qualified Opaleye.QueryArr as Q
 import           Opaleye.QueryArr (Query)
+import qualified Opaleye.Internal.Distinct as D
 import qualified Opaleye.Internal.Unpackspec as U
 import qualified Opaleye.Internal.Tag as T
 import qualified Opaleye.Internal.PrimQuery as PQ
@@ -19,26 +20,5 @@ distinct = distinctExplicit D.def
 
 distinctExplicit :: U.Unpackspec columns columns'
                  -> Query columns -> Query columns'
-distinctExplicit unpack q = Q.simpleQueryArr (distinctU unpack
+distinctExplicit unpack q = Q.simpleQueryArr (D.distinctU unpack
                                               . Q.runSimpleQueryArr q)
-
--- TODO: duplication with runQueryArrUnpack
--- TODO: move this to Internal
--- FIXME: Need to use the tag.  Write a test for this!
-distinctU :: U.Unpackspec columns columns'
-          -> (columns, PQ.PrimQuery, T.Tag) -> (columns', PQ.PrimQuery, T.Tag)
-distinctU unpack (columns, primQ, t) = (newColumns, primQ', t)
-  where -- TODO: This should really be a writer for the writable part
-        f :: HPQ.PrimExpr
-          -> S.State ([(String, Maybe HPQ.AggrOp, HPQ.PrimExpr)], Int)
-                     HPQ.PrimExpr
-        f pe = do
-          (groupPEs, i) <- S.get
-          let s = "result" ++ show i
-          S.put (groupPEs ++ [(s, Nothing, pe)], i+1)
-          return (HPQ.AttrExpr s)
-
-        (newColumns, (groupPEs', _)) =
-          S.runState (U.runUnpackspec unpack f columns) ([], 0)
-
-        primQ' = PQ.Aggregate groupPEs' primQ
---
+++ Opaleye/Internal/Distinct.hs
@@ -0,0 +1,28 @@
+module Opaleye.Internal.Distinct where
+
+import qualified Opaleye.Internal.Unpackspec as U
+import qualified Opaleye.Internal.Tag as T
+import qualified Opaleye.Internal.PrimQuery as PQ
+import qualified Database.HaskellDB.PrimQuery as HPQ
+
+import qualified Control.Monad.Trans.State as S
+
+-- TODO: duplication with runQueryArrUnpack
+-- FIXME: Need to use the tag.  Write a test for this!
+distinctU :: U.Unpackspec columns columns'
+          -> (columns, PQ.PrimQuery, T.Tag) -> (columns', PQ.PrimQuery, T.Tag)
+distinctU unpack (columns, primQ, t) = (newColumns, primQ', t)
+  where -- TODO: This should really be a writer for the writable part
+        f :: HPQ.PrimExpr
+          -> S.State ([(String, Maybe HPQ.AggrOp, HPQ.PrimExpr)], Int)
+                     HPQ.PrimExpr
+        f pe = do
+          (groupPEs, i) <- S.get
+          let s = "result" ++ show i
+          S.put (groupPEs ++ [(s, Nothing, pe)], i+1)
+          return (HPQ.AttrExpr s)
+
+        (newColumns, (groupPEs', _)) =
+          S.runState (U.runUnpackspec unpack f columns) ([], 0)
+
+        primQ' = PQ.Aggregate groupPEs' primQ
