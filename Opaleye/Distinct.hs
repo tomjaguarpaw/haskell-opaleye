fd99f14bda1784985210bb35e61df941bb242a83
Tom Ellis <tom-2013@purelyagile.com>
2014-11-06 21:54:33 +0000

    Remove unused imports

--- Opaleye/Distinct.hs
+++ Opaleye/Distinct.hs
@@ -6,13 +6,8 @@ import qualified Opaleye.QueryArr as Q
 import           Opaleye.QueryArr (Query)
 import qualified Opaleye.Internal.Distinct as D
 import qualified Opaleye.Internal.Unpackspec as U
-import qualified Opaleye.Internal.Tag as T
-import qualified Opaleye.Internal.PrimQuery as PQ
-import qualified Database.HaskellDB.PrimQuery as HPQ
 
 import qualified Data.Profunctor.Product.Default as D
-
-import qualified Control.Monad.Trans.State as S
 
 distinct :: D.Default U.Unpackspec columns columns =>
             Query columns -> Query columns
