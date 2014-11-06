9c72e93cb9c6c1b88d439c1ec3f43a8771c6fd4e
Tom Ellis <tom-2013@purelyagile.com>
2014-11-06 23:28:56 +0000

    Optimize.mergeProduct

--- Opaleye/Internal/NEList.hs
+++ Opaleye/Internal/NEList.hs
@@ -8,5 +8,13 @@ singleton = flip NEList []
 toList :: NEList a -> [a]
 toList (NEList a as) = a:as
 
+neCat :: NEList a -> NEList a -> NEList a
+neCat (NEList a as) bs = NEList a (as ++ toList bs)
+
 instance Functor NEList where
   fmap f (NEList a as) = NEList (f a) (fmap f as)
+
+instance Monad NEList where
+  return = flip NEList []
+  NEList a as >>= f = NEList b (bs ++ (as >>= (toList . f)))
+    where NEList b bs = f a
--- Opaleye/Internal/Optimize.hs
+++ Opaleye/Internal/Optimize.hs
@@ -6,7 +6,7 @@ import qualified Opaleye.Internal.PrimQuery as PQ
 import qualified Opaleye.Internal.NEList as NE
 
 optimize :: PQ.PrimQuery -> PQ.PrimQuery
-optimize = removeUnit
+optimize = mergeProduct . removeUnit
 
 removeUnit :: PQ.PrimQuery -> PQ.PrimQuery
 removeUnit = PQ.foldPrimQuery (PQ.Unit, PQ.BaseTable, product, PQ.Aggregate,
@@ -15,3 +15,14 @@ removeUnit = PQ.foldPrimQuery (PQ.Unit, PQ.BaseTable, product, PQ.Aggregate,
           []   -> NE.NEList PQ.Unit []
           x:xs -> NE.NEList x       xs
           where notUnit = (filter (not . PQ.isUnit) . NE.toList) pqs
+
+mergeProduct :: PQ.PrimQuery -> PQ.PrimQuery
+mergeProduct = PQ.foldPrimQuery (PQ.Unit, PQ.BaseTable, product, PQ.Aggregate,
+                                PQ.Order, PQ.Limit, PQ.LeftJoin)
+  where product pqs pes = PQ.Product pqs' (pes ++ pes')
+          where pqs' = pqs >>= queries
+                queries (PQ.Product qs _) = qs
+                queries q = NE.NEList q []
+                pes' = NE.toList pqs >>= conds
+                conds (PQ.Product _ cs) = cs
+                conds _ = []
