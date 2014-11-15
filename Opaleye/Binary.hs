40b43c9c8c3740f9e947b1cbe4fff5833a578ffd
Tom Ellis <tom-2013@purelyagile.com>
2014-11-15 17:32:29 +0000

    Add Internal.Binary

--- Opaleye/Binary.hs
+++ Opaleye/Binary.hs
@@ -1,30 +1,21 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts #-}
 
 module Opaleye.Binary where
 
 import           Opaleye.QueryArr (Query)
 import qualified Opaleye.QueryArr as Q
-import           Opaleye.Column (Column(Column))
+import qualified Opaleye.Internal.Binary as B
 import qualified Opaleye.Internal.Tag as T
 import qualified Opaleye.Internal.PrimQuery as PQ
 import qualified Opaleye.Internal.PackMap as PM
-import qualified Opaleye.Internal.Values as V
 
-import qualified Database.HaskellDB.PrimQuery as HPQ
-
-import           Data.Profunctor (Profunctor, dimap)
-import           Data.Profunctor.Product (ProductProfunctor, empty, (***!))
-import qualified Data.Profunctor.Product as PP
 import           Data.Profunctor.Product.Default (Default, def)
 
-import           Control.Applicative (Applicative, pure, (<*>))
-import           Control.Arrow ((***))
-
-unionAll :: Default Binaryspec columns columns =>
+unionAll :: Default B.Binaryspec columns columns =>
             Query columns -> Query columns -> Query columns
 unionAll = unionAllExplicit def
 
-unionAllExplicit :: Binaryspec columns columns'
+unionAllExplicit :: B.Binaryspec columns columns'
                  -> Query columns -> Query columns -> Query columns'
 unionAllExplicit binaryspec q1 q2 = Q.simpleQueryArr q where
   q ((), startTag) = (newColumns, newPrimQuery, T.next endTag)
@@ -32,45 +23,7 @@ unionAllExplicit binaryspec q1 q2 = Q.simpleQueryArr q where
           (columns2, primQuery2, endTag) = Q.runSimpleQueryArr q2 ((), midTag)
 
           (newColumns, pes) =
-            PM.run (runBinaryspec binaryspec (extractBinaryFields endTag)
-                                  (columns1, columns2))
+            PM.run (B.runBinaryspec binaryspec (B.extractBinaryFields endTag)
+                                    (columns1, columns2))
 
           newPrimQuery = PQ.Binary PQ.UnionAll pes (primQuery1, primQuery2)
-
-extractBinaryFields :: T.Tag -> (HPQ.PrimExpr, HPQ.PrimExpr)
-                    -> PM.PM [(String, (HPQ.PrimExpr, HPQ.PrimExpr))]
-                             HPQ.PrimExpr
-extractBinaryFields = V.extractAttr ("binary" ++)
-
-data Binaryspec columns columns' =
-  Binaryspec (PM.PackMap (HPQ.PrimExpr, HPQ.PrimExpr) HPQ.PrimExpr
-                         (columns, columns) columns')
-
-runBinaryspec :: Applicative f => Binaryspec columns columns'
-                 -> ((HPQ.PrimExpr, HPQ.PrimExpr) -> f HPQ.PrimExpr)
-                 -> (columns, columns) -> f columns'
-runBinaryspec (Binaryspec b) f = PM.packmap b f
-
-instance Default Binaryspec (Column a) (Column a) where
-  def = Binaryspec (PM.PackMap (\f (Column e, Column e')
-                                -> fmap Column (f (e, e'))))
-
--- {
-
--- Boilerplate instance definitions.  Theoretically, these are derivable.
-
-instance Functor (Binaryspec a) where
-  fmap f (Binaryspec g) = Binaryspec (fmap f g)
-
-instance Applicative (Binaryspec a) where
-  pure = Binaryspec . pure
-  Binaryspec f <*> Binaryspec x = Binaryspec (f <*> x)
-
-instance Profunctor Binaryspec where
-  dimap f g (Binaryspec b) = Binaryspec (dimap (f *** f) g b)
-
-instance ProductProfunctor Binaryspec where
-  empty = PP.defaultEmpty
-  (***!) = PP.defaultProfunctorProduct
-
--- }
---
+++ Opaleye/Internal/Binary.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts #-}
+
+module Opaleye.Internal.Binary where
+
+import           Opaleye.Column (Column(Column))
+import qualified Opaleye.Internal.Tag as T
+import qualified Opaleye.Internal.PackMap as PM
+import qualified Opaleye.Internal.Values as V
+
+import qualified Database.HaskellDB.PrimQuery as HPQ
+
+import           Data.Profunctor (Profunctor, dimap)
+import           Data.Profunctor.Product (ProductProfunctor, empty, (***!))
+import qualified Data.Profunctor.Product as PP
+import           Data.Profunctor.Product.Default (Default, def)
+
+import           Control.Applicative (Applicative, pure, (<*>))
+import           Control.Arrow ((***))
+
+extractBinaryFields :: T.Tag -> (HPQ.PrimExpr, HPQ.PrimExpr)
+                    -> PM.PM [(String, (HPQ.PrimExpr, HPQ.PrimExpr))]
+                             HPQ.PrimExpr
+extractBinaryFields = V.extractAttr ("binary" ++)
+
+data Binaryspec columns columns' =
+  Binaryspec (PM.PackMap (HPQ.PrimExpr, HPQ.PrimExpr) HPQ.PrimExpr
+                         (columns, columns) columns')
+
+runBinaryspec :: Applicative f => Binaryspec columns columns'
+                 -> ((HPQ.PrimExpr, HPQ.PrimExpr) -> f HPQ.PrimExpr)
+                 -> (columns, columns) -> f columns'
+runBinaryspec (Binaryspec b) f = PM.packmap b f
+
+instance Default Binaryspec (Column a) (Column a) where
+  def = Binaryspec (PM.PackMap (\f (Column e, Column e')
+                                -> fmap Column (f (e, e'))))
+
+-- {
+
+-- Boilerplate instance definitions.  Theoretically, these are derivable.
+
+instance Functor (Binaryspec a) where
+  fmap f (Binaryspec g) = Binaryspec (fmap f g)
+
+instance Applicative (Binaryspec a) where
+  pure = Binaryspec . pure
+  Binaryspec f <*> Binaryspec x = Binaryspec (f <*> x)
+
+instance Profunctor Binaryspec where
+  dimap f g (Binaryspec b) = Binaryspec (dimap (f *** f) g b)
+
+instance ProductProfunctor Binaryspec where
+  empty = PP.defaultEmpty
+  (***!) = PP.defaultProfunctorProduct
+
+-- }
