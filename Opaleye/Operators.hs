5e167844e15060e7551f549b2310c6bc5876e43f
Tom Ellis <tom-2013@purelyagile.com>
2014-11-15 22:44:52 +0000

    Add some operators

--- Opaleye/Operators.hs
+++ Opaleye/Operators.hs
@@ -1,6 +1,12 @@
-module Opaleye.Operators (module Opaleye.Operators, (.==), (.>), case_) where
+module Opaleye.Operators (module Opaleye.Operators,
+                          (.==),
+                          (.>),
+                          case_,
+                          ifThenElse) where
 
-import           Opaleye.Internal.Column (Column(Column), (.==), case_, (.>))
+import           Opaleye.Internal.Column (Column(Column), (.==), case_, (.>),
+                                          ifThenElse)
+import qualified Opaleye.Internal.Column as C
 import           Opaleye.QueryArr (QueryArr(QueryArr))
 import qualified Opaleye.Internal.PrimQuery as PQ
 
@@ -12,3 +18,28 @@ restrict = QueryArr f where
 
 doubleOfInt :: Column Int -> Column Double
 doubleOfInt (Column e) = Column (HPQ.CastExpr "double precision" e)
+
+(.<) :: Column a -> Column a -> Column Bool
+(.<) = C.binOp HPQ.OpLt
+
+(.<=) :: Column a -> Column a -> Column Bool
+(.<=) = C.binOp HPQ.OpLtEq
+
+(.>=) :: Column a -> Column a -> Column Bool
+(.>=) = C.binOp HPQ.OpGtEq
+
+(.&&) :: Column Bool -> Column Bool -> Column Bool
+(.&&) = C.binOp HPQ.OpAnd
+
+(.||) :: Column Bool -> Column Bool -> Column Bool
+(.||) = C.binOp HPQ.OpOr
+
+not :: Column Bool -> Column Bool
+not = C.unOp HPQ.OpNot
+
+(.++) :: Column String -> Column String -> Column String
+(.++) = C.binOp (HPQ.OpOther "||")
+
+(./=) :: Column a -> Column a -> Column Bool
+(./=) = C.binOp HPQ.OpNotEq
+
