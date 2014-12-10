6dddaced01a8d073b85d0d024824c0e4caee87aa
Tom Ellis <tom-2013@purelyagile.com>
2014-12-10 19:11:02 +0000

    Add missing top-level reexports. This problem was masked by what is seemingly a cabal bug. https://www.haskell.org/pipermail/haskell-cafe/2014-December/117257.html

--- src/Opaleye.hs
+++ src/Opaleye.hs
@@ -1,20 +1,30 @@
-module Opaleye ( module Opaleye.Column
-               , module Opaleye.Table
-               , module Opaleye.Operators
-               , module Opaleye.PGTypes
-               , module Opaleye.Aggregate
+module Opaleye ( module Opaleye.Aggregate
+               , module Opaleye.Binary
+               , module Opaleye.Column
+               , module Opaleye.Distinct
                , module Opaleye.Join
+               , module Opaleye.Manipulation
+               , module Opaleye.Operators
+               , module Opaleye.Order
+               , module Opaleye.PGTypes
+               , module Opaleye.QueryArr
                , module Opaleye.RunQuery
                , module Opaleye.Sql
-               , module Opaleye.QueryArr )
-       where
+               , module Opaleye.Table
+               , module Opaleye.Values
+               ) where
 
-import Opaleye.Column hiding (null)
-import Opaleye.Table
-import Opaleye.Operators hiding (not)
-import Opaleye.PGTypes
 import Opaleye.Aggregate
+import Opaleye.Binary
+import Opaleye.Column
+import Opaleye.Distinct
 import Opaleye.Join
+import Opaleye.Manipulation
+import Opaleye.Operators
+import Opaleye.Order
+import Opaleye.PGTypes
+import Opaleye.QueryArr
 import Opaleye.RunQuery
 import Opaleye.Sql
-import Opaleye.QueryArr
+import Opaleye.Table
+import Opaleye.Values
