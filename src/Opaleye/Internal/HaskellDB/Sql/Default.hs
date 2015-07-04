da21ec7ec24786ecbe2544c78ab14fd53c014de7
Tom Ellis <tom-2013@purelyagile.com>
2015-07-04 11:29:07 +0100

    Comment about structure of aggregations

--- src/Opaleye/Internal/HaskellDB/Sql/Default.hs
+++ src/Opaleye/Internal/HaskellDB/Sql/Default.hs
@@ -111,6 +111,12 @@ defaultSqlExpr gen expr =
                                 UnOpFun     -> FunSqlExpr op' [e']
                                 UnOpPrefix  -> PrefixSqlExpr op' (ParensSqlExpr e')
                                 UnOpPostfix -> PostfixSqlExpr op' e'
+      -- TODO: The current arrangement whereby the delimeter parameter
+      -- of string_agg is in the AggrStringAggr constructor, but the
+      -- parameter being aggregated is not, seems unsatisfactory
+      -- because it leads to a non-uniformity of treatment, as seen
+      -- below.  Perhaps we should have just `AggrExpr AggrOp` and
+      -- always put the `PrimExpr` in the `AggrOp`.
       AggrExpr op e    -> let op' = showAggrOp op
                               e' = sqlExpr gen e
                               moreAggrFunParams = case op of
