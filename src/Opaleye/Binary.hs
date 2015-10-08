efac85b0b41047197000bc2627af07b79daa4d0d
Andrew Martin <amartin@layer3com.com>
2015-10-08 10:31:45 -0400

    Refactor common code from unionExplicit and unionAllExplicit. Also, fixed the definition of union.

--- src/Opaleye/Binary.hs
+++ src/Opaleye/Binary.hs
@@ -35,27 +35,23 @@ unionAll = unionAllExplicit def
 
 unionAllExplicit :: B.Binaryspec columns columns'
                  -> Query columns -> Query columns -> Query columns'
-unionAllExplicit binaryspec q1 q2 = Q.simpleQueryArr q where
-  q ((), startTag) = (newColumns, newPrimQuery, T.next endTag)
-    where (columns1, primQuery1, midTag) = Q.runSimpleQueryArr q1 ((), startTag)
-          (columns2, primQuery2, endTag) = Q.runSimpleQueryArr q2 ((), midTag)
-
-          (newColumns, pes) =
-            PM.run (B.runBinaryspec binaryspec (B.extractBinaryFields endTag)
-                                    (columns1, columns2))
-
-          newPrimQuery = PQ.Binary PQ.UnionAll pes (primQuery1, primQuery2)
+unionAllExplicit = sameTypeBinOpHelper PQ.UnionAll
 
 
 -- | The same as unionAll, except that it additionally removes any 
 --   duplicate rows.
 union :: Default B.Binaryspec columns columns =>
          Query columns -> Query columns -> Query columns
-union = unionAllExplicit def
+union = unionExplicit def
 
 unionExplicit :: B.Binaryspec columns columns'
               -> Query columns -> Query columns -> Query columns'
-unionExplicit binaryspec q1 q2 = Q.simpleQueryArr q where
+unionExplicit = sameTypeBinOpHelper PQ.Union
+
+
+sameTypeBinOpHelper :: PQ.BinOp -> B.Binaryspec columns columns'
+                    -> Query columns -> Query columns -> Query columns'
+sameTypeBinOpHelper binop binaryspec q1 q2 = Q.simpleQueryArr q where
   q ((), startTag) = (newColumns, newPrimQuery, T.next endTag)
     where (columns1, primQuery1, midTag) = Q.runSimpleQueryArr q1 ((), startTag)
           (columns2, primQuery2, endTag) = Q.runSimpleQueryArr q2 ((), midTag)
@@ -64,6 +60,8 @@ unionExplicit binaryspec q1 q2 = Q.simpleQueryArr q where
             PM.run (B.runBinaryspec binaryspec (B.extractBinaryFields endTag)
                                     (columns1, columns2))
 
-          newPrimQuery = PQ.Binary PQ.Union pes (primQuery1, primQuery2)
+          newPrimQuery = PQ.Binary binop pes (primQuery1, primQuery2)
+  
+
 
 
