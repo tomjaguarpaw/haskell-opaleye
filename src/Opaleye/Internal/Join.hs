827855056e81ef45009e40a6f526fb52443332dd
Tom Ellis <tom-2013@purelyagile.com>
2016-01-30 09:37:02 +0000

    Functor and Applicative instances for NullMaker

--- src/Opaleye/Internal/Join.hs
+++ src/Opaleye/Internal/Join.hs
@@ -7,8 +7,10 @@ import qualified Opaleye.Internal.PackMap as PM
 import           Opaleye.Internal.Column (Column, Nullable)
 import qualified Opaleye.Column as C
 
+import qualified Control.Applicative as A
+
 import           Data.Profunctor (Profunctor, dimap)
-import           Data.Profunctor.Product (ProductProfunctor, empty, (***!))
+import qualified Data.Profunctor.Product as PP
 import qualified Data.Profunctor.Product.Default as D
 
 import qualified Opaleye.Internal.HaskellDB.PrimQuery as HPQ
@@ -30,11 +32,18 @@ instance D.Default NullMaker (Column (Nullable a)) (Column (Nullable a)) where
 
 -- { Boilerplate instances
 
+instance Functor (NullMaker a) where
+  fmap f (NullMaker g) = NullMaker (fmap f g)
+
+instance A.Applicative (NullMaker a) where
+  pure = NullMaker . A.pure
+  NullMaker f <*> NullMaker x = NullMaker (f A.<*> x)
+
 instance Profunctor NullMaker where
   dimap f g (NullMaker h) = NullMaker (dimap f g h)
 
-instance ProductProfunctor NullMaker where
-  empty = NullMaker empty
-  NullMaker f ***! NullMaker f' = NullMaker (f ***! f')
+instance PP.ProductProfunctor NullMaker where
+  empty  = PP.defaultEmpty
+  (***!) = PP.defaultProfunctorProduct
 
 --
