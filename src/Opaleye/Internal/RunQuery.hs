7929f3457d218655cc80a1c1b904c9e1a947e9ac
Tom Ellis <tom-2013@purelyagile.com>
2016-04-27 20:08:22 +0100

    newtype is fine for QueryRunnerColumn

--- src/Opaleye/Internal/RunQuery.hs
+++ src/Opaleye/Internal/RunQuery.hs
@@ -66,7 +66,7 @@ import           Data.Typeable (Typeable)
 -- be that we can't add nullability to a RowParser, only to a
 -- FieldParser, so we have to have some type that we know contains
 -- just a FieldParser.
-data QueryRunnerColumn pgType haskellType =
+newtype QueryRunnerColumn pgType haskellType =
   QueryRunnerColumn (U.Unpackspec (Column pgType) ()) (FieldParser haskellType)
 
 data QueryRunner columns haskells =
