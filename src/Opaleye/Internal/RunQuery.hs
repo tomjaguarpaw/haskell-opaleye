75577fdc3da7e86c1679be96406aaf7562860a41
tomjaguarpaw <tom-github.com@jaguarpaw.co.uk>
2016-11-02 16:12:07 +0000

    Document QueryRunner

--- src/Opaleye/Internal/RunQuery.hs
+++ src/Opaleye/Internal/RunQuery.hs
@@ -72,6 +72,10 @@ data QueryRunnerColumn pgType haskellType =
 instance Functor (QueryRunnerColumn u) where
   fmap f ~(QueryRunnerColumn u fp) = QueryRunnerColumn u ((fmap . fmap . fmap) f fp)
 
+-- | A query runner specifies how to convert Postgres values (@columns@)
+--   into Haskell values (@haskells@).  Most likely you will never need
+--   to create on of these or handle one directly.  It will be provided
+--   for you by the 'D.Default' 'QueryRunner' instance.
 data QueryRunner columns haskells =
   QueryRunner (U.Unpackspec columns ())
               (columns -> RowParser haskells)
