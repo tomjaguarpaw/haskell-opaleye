82a3fd41b9391eb636eea4d56c179767c9c5b124
Tom Ellis <tom-2013@purelyagile.com>
2017-08-20 12:38:05 +0100

    Use newtype instead of data

--- src/Opaleye/Internal/Operators.hs
+++ src/Opaleye/Internal/Operators.hs
@@ -29,8 +29,7 @@ infixr 3 .&&
 (.&&) :: Column T.PGBool -> Column T.PGBool -> Column T.PGBool
 (.&&) = C.binOp HPQ.OpAnd
 
--- Probably should be newtype
-data EqPP a b = EqPP (a -> a -> Column T.PGBool)
+newtype EqPP a b = EqPP (a -> a -> Column T.PGBool)
 
 eqExplicit :: EqPP columns a -> columns -> columns -> Column T.PGBool
 eqExplicit (EqPP f) = f
