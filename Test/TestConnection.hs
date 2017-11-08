5b70bcacbb9cc71a80adef116898f7732f3e798e
Saurabh Nanda <saurabh@vacationlabs.com>
2017-11-08 23:31:07 +0530

    Initial commit for benchmarks
    - Extracted out code for creating a connection to the test DB
    - Added ‘benchmark’ section/target to cabal file

---
+++ Test/TestConnection.hs
@@ -0,0 +1,24 @@
+module TestConnection where
+
+import qualified Database.PostgreSQL.Simple as PGS
+import System.Environment
+import qualified Configuration.Dotenv as Dotenv
+import qualified Data.String as String
+import Control.Applicative ((<|>))
+
+getTestDbConnection :: IO PGS.Connection
+getTestDbConnection = do
+  let envVarName = "POSTGRES_CONNSTRING"
+  connectStringEnvVar <- lookupEnv envVarName
+  connectStringDotEnv <- do vars <- Dotenv.parseFile ".env"
+                            return (lookup envVarName vars)
+                         `Dotenv.onMissingFile`
+                         return Nothing
+  let connectString = connectStringEnvVar <|> connectStringDotEnv
+  conn <- maybe
+          (fail ("Set " ++ envVarName ++ " environment variable\n"
+                 ++ "For example " ++ envVarName ++ "='user=tom dbname=opaleye_test "
+                 ++ "host=localhost port=25433 password=tom'"))
+          (PGS.connectPostgreSQL . String.fromString)
+          connectString
+  pure conn
