5b008b5033056a5b31950f5bb538707253f18bf3
Tom Ellis <tom-2013@purelyagile.com>
2018-04-21 21:13:04 +0100

    Use Select/SelectArr in Opaleye.Distinct

--- src/Opaleye/Distinct.hs
+++ src/Opaleye/Distinct.hs
@@ -3,28 +3,28 @@
 module Opaleye.Distinct (module Opaleye.Distinct, distinctExplicit)
        where
 
-import           Opaleye.QueryArr (Query)
+import           Opaleye.Select (Select)
 import           Opaleye.Internal.Distinct (distinctExplicit, Distinctspec)
 
 import qualified Data.Profunctor.Product.Default as D
 
--- | Remove duplicate rows from the 'Query'.
+-- | Remove duplicate rows from the 'Select'.
 --
 -- Example type specialization:
 --
 -- @
--- distinct :: Query (Column a, Column b) -> Query (Column a, Column b)
+-- distinct :: Select (Column a, Column b) -> Select (Column a, Column b)
 -- @
 --
 -- Assuming the @makeAdaptorAndInstance@ splice has been run for the product type @Foo@:
 --
 -- @
--- distinct :: Query (Foo (Column a) (Column b) (Column c)) -> Query (Foo (Column a) (Column b) (Column c))
+-- distinct :: Select (Foo (Column a) (Column b) (Column c)) -> Select (Foo (Column a) (Column b) (Column c))
 -- @
 --
--- By design there is no @distinct@ function of type @QueryArr a b ->
--- QueryArr a b@.  Such a function would allow violation of SQL's
+-- By design there is no @distinct@ function of type @SelectArr a b ->
+-- SelectArr a b@.  Such a function would allow violation of SQL's
 -- scoping rules and lead to invalid queries.
 distinct :: D.Default Distinctspec columns columns =>
-            Query columns -> Query columns
+            Select columns -> Select columns
 distinct = distinctExplicit D.def
