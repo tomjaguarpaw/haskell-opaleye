e24764059e0dc856af7da6a42dff9a84bf82739b
Tom Ellis <tom-2013@purelyagile.com>
2018-04-21 21:07:47 +0100

    Use Select in Opaleye.Join

--- src/Opaleye/Join.hs
+++ src/Opaleye/Join.hs
@@ -13,10 +13,10 @@
 -- Example specialization:
 --
 -- @
--- leftJoin :: Query (Column a, Column b)
---          -> Query (Column c, Column (Nullable d))
+-- leftJoin :: Select (Column a, Column b)
+--          -> Select (Column c, Column (Nullable d))
 --          -> (((Column a, Column b), (Column c, Column (Nullable d))) -> Column 'Opaleye.SqlTypes.SqlBool')
---          -> Query ((Column a, Column b), (Column (Nullable c), Column (Nullable d)))
+--          -> Select ((Column a, Column b), (Column (Nullable c), Column (Nullable d)))
 -- @
 
 {-# LANGUAGE FlexibleContexts      #-}
@@ -28,8 +28,8 @@ module Opaleye.Join where
 import qualified Opaleye.Internal.Unpackspec as U
 import qualified Opaleye.Internal.Join as J
 import qualified Opaleye.Internal.PrimQuery as PQ
-import           Opaleye.QueryArr (Query, QueryArr)
 import           Opaleye.Internal.Column (Column)
+import qualified Opaleye.Select   as S
 import qualified Opaleye.SqlTypes as T
 
 import qualified Data.Profunctor.Product.Default as D
@@ -39,19 +39,19 @@ import qualified Data.Profunctor.Product.Default as D
 leftJoin  :: (D.Default U.Unpackspec columnsL columnsL,
               D.Default U.Unpackspec columnsR columnsR,
               D.Default J.NullMaker columnsR nullableColumnsR)
-          => Query columnsL  -- ^ Left query
-          -> Query columnsR  -- ^ Right query
+          => S.Select columnsL  -- ^ Left query
+          -> S.Select columnsR  -- ^ Right query
           -> ((columnsL, columnsR) -> Column T.SqlBool) -- ^ Condition on which to join
-          -> Query (columnsL, nullableColumnsR) -- ^ Left join
+          -> S.Select (columnsL, nullableColumnsR) -- ^ Left join
 leftJoin = leftJoinExplicit D.def D.def D.def
 
 -- | 'leftJoinA' is a convenient way of using left joins within arrow
 -- notation
 leftJoinA :: (D.Default U.Unpackspec columnsR columnsR,
               D.Default J.NullMaker columnsR nullableColumnsR)
-          => Query columnsR
+          => S.Select columnsR
           -- ^ Right query
-          -> QueryArr (columnsR -> Column T.SqlBool) nullableColumnsR
+          -> S.SelectArr (columnsR -> Column T.SqlBool) nullableColumnsR
           -- ^ Condition on which to join goes in, left join
           -- result comes out
 leftJoinA = leftJoinAExplict D.def D.def
@@ -59,10 +59,10 @@ leftJoinA = leftJoinAExplict D.def D.def
 rightJoin  :: (D.Default U.Unpackspec columnsL columnsL,
                D.Default U.Unpackspec columnsR columnsR,
                D.Default J.NullMaker columnsL nullableColumnsL)
-           => Query columnsL -- ^ Left query
-           -> Query columnsR -- ^ Right query
+           => S.Select columnsL -- ^ Left query
+           -> S.Select columnsR -- ^ Right query
            -> ((columnsL, columnsR) -> Column T.SqlBool) -- ^ Condition on which to join
-           -> Query (nullableColumnsL, columnsR) -- ^ Right join
+           -> S.Select (nullableColumnsL, columnsR) -- ^ Right join
 rightJoin = rightJoinExplicit D.def D.def D.def
 
 
@@ -70,10 +70,10 @@ fullJoin  :: (D.Default U.Unpackspec columnsL columnsL,
               D.Default U.Unpackspec columnsR columnsR,
               D.Default J.NullMaker columnsL nullableColumnsL,
               D.Default J.NullMaker columnsR nullableColumnsR)
-          => Query columnsL -- ^ Left query
-          -> Query columnsR -- ^ Right query
+          => S.Select columnsL -- ^ Left query
+          -> S.Select columnsR -- ^ Right query
           -> ((columnsL, columnsR) -> Column T.SqlBool) -- ^ Condition on which to join
-          -> Query (nullableColumnsL, nullableColumnsR) -- ^ Full outer join
+          -> S.Select (nullableColumnsL, nullableColumnsR) -- ^ Full outer join
 fullJoin = fullJoinExplicit D.def D.def D.def D.def
 
 -- * Explicit versions
@@ -81,24 +81,24 @@ fullJoin = fullJoinExplicit D.def D.def D.def D.def
 leftJoinExplicit :: U.Unpackspec columnsL columnsL
                  -> U.Unpackspec columnsR columnsR
                  -> J.NullMaker columnsR nullableColumnsR
-                 -> Query columnsL -> Query columnsR
+                 -> S.Select columnsL -> S.Select columnsR
                  -> ((columnsL, columnsR) -> Column T.SqlBool)
-                 -> Query (columnsL, nullableColumnsR)
+                 -> S.Select (columnsL, nullableColumnsR)
 leftJoinExplicit uA uB nullmaker =
   J.joinExplicit uA uB id (J.toNullable nullmaker) PQ.LeftJoin
 
 leftJoinAExplict :: U.Unpackspec columnsR columnsR
                  -> J.NullMaker columnsR nullableColumnsR
-                 -> Query columnsR
-                 -> QueryArr (columnsR -> Column T.SqlBool) nullableColumnsR
+                 -> S.Select columnsR
+                 -> S.SelectArr (columnsR -> Column T.SqlBool) nullableColumnsR
 leftJoinAExplict = J.leftJoinAExplicit
 
 rightJoinExplicit :: U.Unpackspec columnsL columnsL
                   -> U.Unpackspec columnsR columnsR
                   -> J.NullMaker columnsL nullableColumnsL
-                  -> Query columnsL -> Query columnsR
+                  -> S.Select columnsL -> S.Select columnsR
                   -> ((columnsL, columnsR) -> Column T.SqlBool)
-                  -> Query (nullableColumnsL, columnsR)
+                  -> S.Select (nullableColumnsL, columnsR)
 rightJoinExplicit uA uB nullmaker =
   J.joinExplicit uA uB (J.toNullable nullmaker) id PQ.RightJoin
 
@@ -107,8 +107,8 @@ fullJoinExplicit :: U.Unpackspec columnsL columnsL
                  -> U.Unpackspec columnsR columnsR
                  -> J.NullMaker columnsL nullableColumnsL
                  -> J.NullMaker columnsR nullableColumnsR
-                 -> Query columnsL -> Query columnsR
+                 -> S.Select columnsL -> S.Select columnsR
                  -> ((columnsL, columnsR) -> Column T.SqlBool)
-                 -> Query (nullableColumnsL, nullableColumnsR)
+                 -> S.Select (nullableColumnsL, nullableColumnsR)
 fullJoinExplicit uA uB nullmakerA nullmakerB =
   J.joinExplicit uA uB (J.toNullable nullmakerA) (J.toNullable nullmakerB) PQ.FullJoin
