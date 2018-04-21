ce508e94b59f06a95fd71b051b7802a3e78a1e25
Tom Ellis <tom-2013@purelyagile.com>
2018-04-21 13:41:09 +0100

    Use Field in Opaleye.Operators

--- src/Opaleye/Operators.hs
+++ src/Opaleye/Operators.hs
@@ -12,6 +12,7 @@ import qualified Control.Arrow as A
 import qualified Data.Foldable as F
 import qualified Data.List.NonEmpty as NEL
 
+import qualified Opaleye.Field as F
 import           Opaleye.Internal.Column (Column(Column), unsafeCase_,
                                           unsafeIfThenElse, unsafeGt)
 import qualified Opaleye.Internal.Column as C
@@ -46,7 +47,7 @@ then 'keepWhen' will suit you better.
 (If you are familiar with 'Control.Monad.MonadPlus' or
 'Control.Applicative.Alternative' it may help you to know that
 'restrict' corresponds to the 'Control.Monad.guard' function.) -}
-restrict :: S.SelectArr (Column T.SqlBool) ()
+restrict :: S.SelectArr (F.Field T.SqlBool) ()
 restrict = QueryArr f where
   f (Column predicate, primQ, t0) = ((), PQ.restrict predicate primQ, t0)
 
@@ -72,7 +73,7 @@ notation then 'restrict' will suit you better.
 This is the 'S.SelectArr' equivalent of 'Prelude.filter' from the
 'Prelude'.
 -}
-keepWhen :: (a -> Column T.SqlBool) -> S.SelectArr a a
+keepWhen :: (a -> F.Field T.SqlBool) -> S.SelectArr a a
 keepWhen p = proc a -> do
   restrict  -< p a
   A.returnA -< a
@@ -80,43 +81,43 @@ keepWhen p = proc a -> do
 -- * Equality operators
 
 infix 4 .==
-(.==) :: Column a -> Column a -> Column T.SqlBool
+(.==) :: Column a -> Column a -> F.Field T.SqlBool
 (.==) = C.binOp (HPQ.:==)
 
 infix 4 ./=
-(./=) :: Column a -> Column a -> Column T.SqlBool
+(./=) :: Column a -> Column a -> F.Field T.SqlBool
 (./=) = C.binOp (HPQ.:<>)
 
 infix 4 .===
 -- | A polymorphic equality operator that works for all types that you
 -- have run `makeAdaptorAndInstance` on.  This may be unified with
 -- `.==` in a future version.
-(.===) :: D.Default O.EqPP columns columns => columns -> columns -> Column T.SqlBool
+(.===) :: D.Default O.EqPP columns columns => columns -> columns -> F.Field T.SqlBool
 (.===) = (O..==)
 
 infix 4 ./==
 -- | A polymorphic inequality operator that works for all types that
 -- you have run `makeAdaptorAndInstance` on.  This may be unified with
 -- `./=` in a future version.
-(./==) :: D.Default O.EqPP columns columns => columns -> columns -> Column T.SqlBool
+(./==) :: D.Default O.EqPP columns columns => columns -> columns -> F.Field T.SqlBool
 (./==) = Opaleye.Operators.not .: (O..==)
 
 -- * Comparison operators
 
 infix 4 .>
-(.>) :: Ord.SqlOrd a => Column a -> Column a -> Column T.SqlBool
+(.>) :: Ord.SqlOrd a => Column a -> Column a -> F.Field T.SqlBool
 (.>) = unsafeGt
 
 infix 4 .<
-(.<) :: Ord.SqlOrd a => Column a -> Column a -> Column T.SqlBool
+(.<) :: Ord.SqlOrd a => Column a -> Column a -> F.Field T.SqlBool
 (.<) = C.binOp (HPQ.:<)
 
 infix 4 .<=
-(.<=) :: Ord.SqlOrd a => Column a -> Column a -> Column T.SqlBool
+(.<=) :: Ord.SqlOrd a => Column a -> Column a -> F.Field T.SqlBool
 (.<=) = C.binOp (HPQ.:<=)
 
 infix 4 .>=
-(.>=) :: Ord.SqlOrd a => Column a -> Column a -> Column T.SqlBool
+(.>=) :: Ord.SqlOrd a => Column a -> Column a -> F.Field T.SqlBool
 (.>=) = C.binOp (HPQ.:>=)
 
 -- * Numerical operators
@@ -135,18 +136,18 @@ rem_ = C.binOp HPQ.OpMod
 -- * Conditional operators
 
 -- | Select the first case for which the condition is true.
-case_ :: [(Column T.SqlBool, Column a)] -> Column a -> Column a
+case_ :: [(F.Field T.SqlBool, Column a)] -> Column a -> Column a
 case_ = unsafeCase_
 
 -- | Monomorphic if\/then\/else.
 --
 -- This may be replaced by 'ifThenElseMany' in a future version.
-ifThenElse :: Column T.SqlBool -> Column a -> Column a -> Column a
+ifThenElse :: F.Field T.SqlBool -> Column a -> Column a -> Column a
 ifThenElse = unsafeIfThenElse
 
 -- | Polymorphic if\/then\/else.
 ifThenElseMany :: D.Default O.IfPP columns columns
-               => Column T.SqlBool
+               => F.Field T.SqlBool
                -> columns
                -> columns
                -> columns
@@ -157,43 +158,43 @@ ifThenElseMany = O.ifExplict D.def
 infixr 2 .||
 
 -- | Boolean or
-(.||) :: Column T.SqlBool -> Column T.SqlBool -> Column T.SqlBool
+(.||) :: F.Field T.SqlBool -> F.Field T.SqlBool -> F.Field T.SqlBool
 (.||) = C.binOp HPQ.OpOr
 
 infixr 3 .&&
 
 -- | Boolean and
-(.&&) :: Column T.SqlBool -> Column T.SqlBool -> Column T.SqlBool
+(.&&) :: F.Field T.SqlBool -> F.Field T.SqlBool -> F.Field T.SqlBool
 (.&&) = (O..&&)
 
 -- | Boolean not
-not :: Column T.SqlBool -> Column T.SqlBool
+not :: F.Field T.SqlBool -> F.Field T.SqlBool
 not = C.unOp HPQ.OpNot
 
 -- | True when any element of the container is true
-ors :: F.Foldable f => f (Column T.SqlBool) -> Column T.SqlBool
+ors :: F.Foldable f => f (F.Field T.SqlBool) -> F.Field T.SqlBool
 ors = F.foldl' (.||) (T.sqlBool False)
 
 -- * Text operators
 
--- | Concatenate 'Column' 'T.SqlText'
-(.++) :: Column T.SqlText -> Column T.SqlText -> Column T.SqlText
+-- | Concatenate 'F.Field' 'T.SqlText'
+(.++) :: F.Field T.SqlText -> F.Field T.SqlText -> F.Field T.SqlText
 (.++) = C.binOp (HPQ.:||)
 
 -- | To lowercase
-lower :: Column T.SqlText -> Column T.SqlText
+lower :: F.Field T.SqlText -> F.Field T.SqlText
 lower = C.unOp HPQ.OpLower
 
 -- | To uppercase
-upper :: Column T.SqlText -> Column T.SqlText
+upper :: F.Field T.SqlText -> F.Field T.SqlText
 upper = C.unOp HPQ.OpUpper
 
 -- | Postgres @LIKE@ operator
-like :: Column T.SqlText -> Column T.SqlText -> Column T.SqlBool
+like :: F.Field T.SqlText -> F.Field T.SqlText -> F.Field T.SqlBool
 like = C.binOp HPQ.OpLike
 
 -- | Postgres @ILIKE@ operator
-ilike :: Column T.SqlText -> Column T.SqlText -> Column T.SqlBool
+ilike :: F.Field T.SqlText -> F.Field T.SqlText -> F.Field T.SqlBool
 ilike = C.binOp HPQ.OpILike
 
 charLength :: C.PGString a => Column a -> Column Int
@@ -206,7 +207,7 @@ charLength (Column e) = Column (HPQ.FunExpr "char_length" [e])
 -- 'in_' @validProducts@ @product@ checks whether @product@ is a valid
 -- product.  'in_' @validProducts@ is a function which checks whether
 -- a product is a valid product.
-in_ :: (Functor f, F.Foldable f) => f (Column a) -> Column a -> Column T.SqlBool
+in_ :: (Functor f, F.Foldable f) => f (Column a) -> Column a -> F.Field T.SqlBool
 in_ fcas (Column a) = Column $ case NEL.nonEmpty (F.toList fcas) of
    Nothing -> HPQ.ConstExpr (HPQ.BoolLit False)
    Just xs -> HPQ.BinExpr HPQ.OpIn a (HPQ.ListExpr (fmap C.unColumn xs))
@@ -218,7 +219,7 @@ in_ fcas (Column a) = Column $ case NEL.nonEmpty (F.toList fcas) of
 -- expediency, is currently implemented using a @LEFT JOIN@.  Please
 -- file a bug if this causes any issues in practice.
 inQuery :: D.Default O.EqPP columns columns
-        => columns -> S.Select columns -> S.Select (Column T.SqlBool)
+        => columns -> Query columns -> S.Select (F.Field T.SqlBool)
 inQuery c q = qj'
   where -- Remove every row that isn't equal to c
         -- Replace the ones that are with '1'
@@ -229,13 +230,13 @@ inQuery c q = qj'
         -- Left join with a query that has a single row
         -- We either get a single row with '1'
         -- or a single row with 'NULL'
-        qj :: Query (Column T.SqlInt4, Column (C.Nullable T.SqlInt4))
+        qj :: Query (F.Field T.SqlInt4, Column (C.Nullable T.SqlInt4))
         qj = Join.leftJoin (A.arr (const 1))
                            (Distinct.distinct q')
                            (uncurry (.==))
 
         -- Check whether it is 'NULL'
-        qj' :: Query (Column T.SqlBool)
+        qj' :: Query (F.Field T.SqlBool)
         qj' = A.arr (Opaleye.Operators.not
                      . Column.isNull
                      . snd)
@@ -268,58 +269,62 @@ instance PGJsonIndex T.SqlText
 -- | Get JSON object field by key.
 infixl 8 .->
 (.->) :: (SqlIsJson a, SqlJsonIndex k)
-      => Column (C.Nullable a) -- ^
-      -> Column k -- ^ key or index
-      -> Column (C.Nullable a)
+      => F.FieldNullable a -- ^
+      -> F.Field k -- ^ key or index
+      -> F.FieldNullable a
 (.->) = C.binOp (HPQ.:->)
 
 -- | Get JSON object field as text.
 infixl 8 .->>
 (.->>) :: (SqlIsJson a, SqlJsonIndex k)
-       => Column (C.Nullable a) -- ^
-       -> Column k -- ^ key or index
-       -> Column (C.Nullable T.SqlText)
+       => F.FieldNullable a -- ^
+       -> F.Field k -- ^ key or index
+       -> F.FieldNullable T.SqlText
 (.->>) = C.binOp (HPQ.:->>)
 
 -- | Get JSON object at specified path.
 infixl 8 .#>
 (.#>) :: (SqlIsJson a)
-      => Column (C.Nullable a) -- ^
+      => F.FieldNullable a -- ^
       -> Column (T.SqlArray T.SqlText) -- ^ path
-      -> Column (C.Nullable a)
+      -> F.FieldNullable a
 (.#>) = C.binOp (HPQ.:#>)
 
 -- | Get JSON object at specified path as text.
 infixl 8 .#>>
 (.#>>) :: (SqlIsJson a)
-       => Column (C.Nullable a) -- ^
+       => F.FieldNullable a -- ^
        -> Column (T.SqlArray T.SqlText) -- ^ path
-       -> Column (C.Nullable T.SqlText)
+       -> F.FieldNullable T.SqlText
 (.#>>) = C.binOp (HPQ.:#>>)
 
 -- | Does the left JSON value contain within it the right value?
 infix 4 .@>
-(.@>) :: Column T.SqlJsonb -> Column T.SqlJsonb -> Column T.SqlBool
+(.@>) :: F.Field T.SqlJsonb -> F.Field T.SqlJsonb -> F.Field T.SqlBool
 (.@>) = C.binOp (HPQ.:@>)
 
 -- | Is the left JSON value contained within the right value?
 infix 4 .<@
-(.<@) :: Column T.SqlJsonb -> Column T.SqlJsonb -> Column T.SqlBool
+(.<@) :: F.Field T.SqlJsonb -> F.Field T.SqlJsonb -> F.Field T.SqlBool
 (.<@) = C.binOp (HPQ.:<@)
 
 -- | Does the key/element string exist within the JSON value?
 infix 4 .?
-(.?) :: Column T.SqlJsonb -> Column T.SqlText -> Column T.SqlBool
+(.?) :: F.Field T.SqlJsonb -> F.Field T.SqlText -> F.Field T.SqlBool
 (.?) = C.binOp (HPQ.:?)
 
 -- | Do any of these key/element strings exist?
 infix 4 .?|
-(.?|) :: Column T.SqlJsonb -> Column (T.SqlArray T.SqlText) -> Column T.SqlBool
+(.?|) :: F.Field T.SqlJsonb
+      -> Column (T.SqlArray T.SqlText)
+      -> F.Field T.SqlBool
 (.?|) = C.binOp (HPQ.:?|)
 
 -- | Do all of these key/element strings exist?
 infix 4 .?&
-(.?&) :: Column T.SqlJsonb -> Column (T.SqlArray T.SqlText) -> Column T.SqlBool
+(.?&) :: F.Field T.SqlJsonb
+      -> Column (T.SqlArray T.SqlText)
+      -> F.Field T.SqlBool
 (.?&) = C.binOp (HPQ.:?&)
 
 -- * SqlArray operators
@@ -338,37 +343,37 @@ index (Column a) (Column b) = Column (HPQ.ArrayIndex a b)
 
 -- * Range operators
 
-overlap :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+overlap :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 overlap = C.binOp (HPQ.:&&)
 
-liesWithin :: T.IsRangeType a => Column a -> Column (T.SqlRange a) -> Column T.SqlBool
+liesWithin :: T.IsRangeType a => Column a -> Column (T.SqlRange a) -> F.Field T.SqlBool
 liesWithin = C.binOp (HPQ.:<@)
 
 infix 4 .<<
-(.<<) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+(.<<) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 (.<<) = C.binOp (HPQ.:<<)
 
 infix 4 .>>
-(.>>) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+(.>>) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 (.>>) = C.binOp (HPQ.:>>)
 
 infix 4 .&<
-(.&<) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+(.&<) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 (.&<) = C.binOp (HPQ.:&<)
 
 infix 4 .&>
-(.&>) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+(.&>) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 (.&>) = C.binOp (HPQ.:&>)
 
 infix 4 .-|-
-(.-|-) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> Column T.SqlBool
+(.-|-) :: Column (T.SqlRange a) -> Column (T.SqlRange a) -> F.Field T.SqlBool
 (.-|-) = C.binOp (HPQ.:-|-)
 
 -- * Other operators
 
-timestamptzAtTimeZone :: Column T.SqlTimestamptz
-                      -> Column T.SqlText
-                      -> Column T.SqlTimestamp
+timestamptzAtTimeZone :: F.Field T.SqlTimestamptz
+                      -> F.Field T.SqlText
+                      -> F.Field T.SqlTimestamp
 timestamptzAtTimeZone = C.binOp HPQ.OpAtTimeZone
 
 -- * Deprecated
@@ -376,7 +381,7 @@ timestamptzAtTimeZone = C.binOp HPQ.OpAtTimeZone
 {-# DEPRECATED doubleOfInt
     "Use 'C.unsafeCast' instead. \
     \Will be removed in version 0.7." #-}
-doubleOfInt :: Column T.SqlInt4 -> Column T.SqlFloat8
+doubleOfInt :: F.Field T.SqlInt4 -> F.Field T.SqlFloat8
 doubleOfInt (Column e) = Column (HPQ.CastExpr "float8" e)
 
 -- | Identical to 'restrictExists'.  Will be deprecated in version 0.7.
