d18090e5d9b7510be35dada16bc3116241cc5f01
Maciej Bielecki <zyla@prati.pl>
2018-05-18 14:37:00 +0200

    Oops, broken GHC 8.2.2 build

--- src/Opaleye/Internal/Table.hs
+++ src/Opaleye/Internal/Table.hs
@@ -19,6 +19,7 @@ import           Data.Profunctor.Product (ProductProfunctor, empty, (***!))
 import qualified Data.Profunctor.Product as PP
 import qualified Data.List.NonEmpty as NEL
 import           Data.Monoid (Monoid, mempty, mappend)
+import           Data.Semigroup (Semigroup((<>)))
 import           Control.Applicative (Applicative, pure, (<*>), liftA2)
 import qualified Control.Arrow as Arr
 
