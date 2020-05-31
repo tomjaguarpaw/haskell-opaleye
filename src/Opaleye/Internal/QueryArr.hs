1c0c3ed503b21b65228dee7a526c81904402ecdd
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-05-31 15:46:20 +0100

    Finally, a Monad instance for queries!

--- src/Opaleye/Internal/QueryArr.hs
+++ src/Opaleye/Internal/QueryArr.hs
@@ -76,12 +76,19 @@ instance Arr.ArrowChoice QueryArr where
             Left a -> first3 Left (runQueryArr f (a, primQ, t0))
             Right b -> (Right b, primQ, t0)
 
+instance Arr.ArrowApply QueryArr where
+  app = lateral (\(f, i) -> f <<< pure i)
+
 instance Functor (QueryArr a) where
   fmap f = (arr f <<<)
 
 instance Applicative (QueryArr a) where
   pure = arr . const
   f <*> g = arr (uncurry ($)) <<< (f &&& g)
+
+instance Monad (QueryArr a) where
+  return = pure
+  as >>= f = lateral (\(i, a) -> f a <<< pure i) <<< (id &&& as)
 
 instance P.Profunctor QueryArr where
   dimap f g a = arr g <<< a <<< arr f
