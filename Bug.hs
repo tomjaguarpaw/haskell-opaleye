285bbb1fe9fb36f3c68bae3bfde14b11145f8b16
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-06-14 12:56:58 +0100

    Even more minimal

--- Bug.hs
+++ Bug.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE Arrows #-}
+
 module Bug where
+
+import qualified Data.Profunctor.Product as PP
+
+chooseChoice :: (a -> i) -> f i -> f a
+chooseChoice choose fi = _ $ proc a -> do
+  case choose a of
+    i  -> fi -< i
