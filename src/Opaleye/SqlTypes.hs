e56e51286b0095c8927199795270a8848094111f
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-06-14 22:52:42 +0100

    Clarify comment

--- src/Opaleye/SqlTypes.hs
+++ src/Opaleye/SqlTypes.hs
@@ -1,5 +1,5 @@
 -- | SQL types and functions to create 'Opaleye.Field.Field_'s of
--- those types.  You may find it more convenient to use
+-- those types.  To create fields you may find it more convenient to use
 -- "Opaleye.ToFields" instead.
 
 module Opaleye.SqlTypes (module Opaleye.SqlTypes,
