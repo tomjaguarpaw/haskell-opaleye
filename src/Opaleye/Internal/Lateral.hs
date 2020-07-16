7259c0b4a090d5955c27377a56762519cdcab4ba
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-07-16 16:35:59 +0100

    Add viaLateral

--- src/Opaleye/Internal/Lateral.hs
+++ src/Opaleye/Internal/Lateral.hs
@@ -3,6 +3,7 @@
 
 module Opaleye.Internal.Lateral
   ( lateral
+  , viaLateral
   , laterally
   , bilaterally
   , bind
@@ -35,11 +36,14 @@ lateral f = Q.QueryArr qa
                                     :| [(PQ.Lateral, primQueryR)])
                                    []
 
+viaLateral :: SelectArr i a -> i -> Select a
+viaLateral s i = s <<< pure i
+
 -- | Lifts operations like 'Opaleye.Aggregate.aggregate',
 -- 'Opaleye.Order.orderBy' and 'Opaleye.Order.limit', which are restricted to
 -- 'Select' normally, to operate on 'SelectArr's taking arbitrary inputs.
 laterally :: (Select a -> Select b) -> SelectArr i a -> SelectArr i b
-laterally f as = lateral (\i -> f (as <<< pure i))
+laterally f as = lateral (\i -> f (viaLateral as i))
 
 
 -- | Lifts operations like 'Opaleye.Binary.union', 'Opaleye.Binary.intersect'
@@ -47,10 +51,10 @@ laterally f as = lateral (\i -> f (as <<< pure i))
 -- operate on 'SelectArr's taking arbitrary inputs.
 bilaterally :: (Select a -> Select b -> Select c)
             -> SelectArr i a -> SelectArr i b -> SelectArr i c
-bilaterally f as bs = lateral (\i -> f (as <<< pure i) (bs <<< pure i))
+bilaterally f as bs = lateral (\i -> f (viaLateral as i) (viaLateral bs i))
 
 bind :: SelectArr i a -> (a -> SelectArr i b) -> SelectArr i b
 bind s f = proc i -> do
   a <- s -< i
-  b <- lateral (\(a, i) -> f a <<< pure i) -< (a, i)
+  b <- lateral (\(a, i) -> viaLateral (f a) i) -< (a, i)
   returnA -< b
