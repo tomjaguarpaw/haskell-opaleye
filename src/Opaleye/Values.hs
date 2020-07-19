d70ed118b73d4dabb3e310c9fddeef80d844369a
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-07-19 12:36:51 +0100

    Better exports for Opaleye.Values

--- src/Opaleye/Values.hs
+++ src/Opaleye/Values.hs
@@ -1,6 +1,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Opaleye.Values where
+module Opaleye.Values(
+  valuesSafe,
+  -- * Explicit versions
+  valuesExplicit,
+  valuesSafeExplicit,
+  valuesUnsafeExplicit,
+  -- * Adaptors
+  V.ValuesspecSafe,
+  V.valuesspecField,
+  -- * Deprecated versions
+  values,
+  valuesUnsafe,
+  ) where
 
 import qualified Opaleye.Internal.QueryArr as Q
 import           Opaleye.Internal.Values as V
@@ -9,26 +21,10 @@ import qualified Opaleye.Select              as S
 
 import           Data.Profunctor.Product.Default (Default, def)
 
--- | 'values' implements Postgres's @VALUES@ construct and allows you
--- to create a @SELECT@ that consists of the given rows.
---
--- Example type specialization:
---
--- @
--- values :: [(Field a, Field b)] -> Select (Field a, Field b)
--- @
---
--- Assuming the @makeAdaptorAndInstance@ splice has been run for the
--- product type @Foo@:
---
--- @
--- values :: [Foo (Field a) (Field b) (Field c)] -> S.Select (Foo (Field a) (Field b) (Field c))
--- @
---
--- (Please note that 'values' of an empty list generates incorrect
+-- | Please note that 'values' of an empty list generates incorrect
 -- queries when mixed with @OUTER@\/@LEFT@\/@RIGHT JOIN@.  You should
 -- use 'valuesSafe' instead.  'valuesSafe' will replace 'values' in
--- version 0.7.)
+-- version 0.7.
 values :: (Default V.Valuesspec fields fields,
            Default U.Unpackspec fields fields) =>
           [fields] -> S.Select fields
@@ -40,6 +36,21 @@ valuesExplicit :: U.Unpackspec fields fields'
 valuesExplicit unpack valuesspec fields =
   Q.productQueryArr (V.valuesU unpack valuesspec fields)
 
+-- | 'valuesSafe' implements Postgres's @VALUES@ construct and allows you
+-- to create a @SELECT@ that consists of the given rows.
+--
+-- Example type specialization:
+--
+-- @
+-- valuesSafe :: [(Field a, Field b)] -> Select (Field a, Field b)
+-- @
+--
+-- Assuming the @makeAdaptorAndInstance@ splice has been run for the
+-- product type @Foo@:
+--
+-- @
+-- valuesSafe :: [Foo (Field a) (Field b) (Field c)] -> S.Select (Foo (Field a) (Field b) (Field c))
+-- @
 valuesSafe :: Default V.ValuesspecSafe fields fields =>
                [fields] -> S.Select fields
 valuesSafe = valuesSafeExplicit def
@@ -49,11 +60,15 @@ valuesSafeExplicit :: V.ValuesspecSafe fields fields'
 valuesSafeExplicit valuesspec fields =
   Q.productQueryArr (V.valuesUSafe valuesspec fields)
 
+-- | Forward-compatible version of unsafe 'values' that will not be
+-- deprecated in 0.7, but in 0.8.
 valuesUnsafe :: (Default V.Valuesspec fields fields,
                  Default U.Unpackspec fields fields) =>
                 [fields] -> S.Select fields
 valuesUnsafe = values
 
+-- | Forward compatible version of unsafe 'valuesExplicit' that will
+-- not be deprecated in 0.7, but in 0.8.
 valuesUnsafeExplicit :: U.Unpackspec fields fields'
                      -> V.Valuesspec fields fields'
                      -> [fields] -> S.Select fields'
