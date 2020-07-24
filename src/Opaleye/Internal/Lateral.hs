1e777dbc1cec5ce8d42222bd08ca848ed3cd76ab
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-07-24 17:14:18 +0100

    Move lateral, viaLateral, bind and arrowApply to Opaleye.Internal.QueryArr

--- src/Opaleye/Internal/Lateral.hs
+++ src/Opaleye/Internal/Lateral.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE Arrows #-}
 
 module Opaleye.Internal.Lateral
   ( lateral
@@ -11,34 +10,7 @@ module Opaleye.Internal.Lateral
   )
 where
 
-import qualified Opaleye.Internal.QueryArr as Q
-import qualified Opaleye.Internal.PrimQuery as PQ
-import           Opaleye.Select
-
-import           Data.List.NonEmpty ( NonEmpty((:|)) )
-import           Control.Arrow ( returnA )
-import           Control.Category ( (<<<) )
-
-
--- | Implements @LATERAL@ subqueries.
---
--- @LATERAL@ gives us goodies like 'Monad' and 'Control.Arrow.ArrowApply'
--- instances for 'SelectArr'. It also allows us to bypass the scoping rules
--- of SQL that otherwise restrict operations like
--- 'Opaleye.Aggregate.aggregate' and 'Opaleye.Binary.union' to 'Select's
--- rather than 'SelectArr's.
-lateral :: (i -> Select a) -> SelectArr i a
-lateral f = Q.QueryArr qa
-  where
-    qa (i, primQueryL, tag) = (a, primQueryJoin, tag')
-      where
-        (a, primQueryR, tag') = Q.runSimpleQueryArr (f i) ((), tag)
-        primQueryJoin = PQ.Product ((PQ.NonLateral, primQueryL)
-                                    :| [(PQ.Lateral, primQueryR)])
-                                   []
-
-viaLateral :: SelectArr i a -> i -> Select a
-viaLateral s i = s <<< pure i
+import           Opaleye.Internal.QueryArr
 
 -- | Lifts operations like 'Opaleye.Aggregate.aggregate',
 -- 'Opaleye.Order.orderBy' and 'Opaleye.Order.limit', which are restricted to
@@ -53,12 +25,3 @@ laterally f as = lateral (\i -> f (viaLateral as i))
 bilaterally :: (Select a -> Select b -> Select c)
             -> SelectArr i a -> SelectArr i b -> SelectArr i c
 bilaterally f as bs = lateral (\i -> f (viaLateral as i) (viaLateral bs i))
-
-bind :: SelectArr i a -> (a -> SelectArr i b) -> SelectArr i b
-bind s f = proc i -> do
-  a <- s -< i
-  b <- lateral (\(a, i) -> viaLateral (f a) i) -< (a, i)
-  returnA -< b
-
-arrowApply :: SelectArr (SelectArr i a, i) a
-arrowApply = lateral (\(f, i) -> viaLateral f i)
--- src/Opaleye/Internal/QueryArr.hs
+++ src/Opaleye/Internal/QueryArr.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Arrows #-}
+
 module Opaleye.Internal.QueryArr where
 
 import           Prelude hiding (id)
@@ -10,10 +12,11 @@ import qualified Opaleye.Internal.PrimQuery as PQ
 import qualified Opaleye.Internal.HaskellDB.PrimQuery as HPQ
 
 import qualified Control.Arrow as Arr
-import           Control.Arrow ((&&&), (***), arr)
+import           Control.Arrow ((&&&), (***), arr, returnA)
 import qualified Control.Category as C
 import           Control.Category ((<<<), id)
 import           Control.Applicative (Applicative, pure, (<*>))
+import           Data.List.NonEmpty ( NonEmpty((:|)) )
 import qualified Data.Profunctor as P
 import qualified Data.Profunctor.Product as PP
 
@@ -61,6 +64,35 @@ type Select = SelectArr ()
 --
 -- @SelectArr a b@ is analogous to a Haskell function @a -> [b]@.
 type SelectArr = QueryArr
+
+-- | Implements @LATERAL@ subqueries.
+--
+-- @LATERAL@ gives us goodies like 'Monad' and 'Control.Arrow.ArrowApply'
+-- instances for 'SelectArr'. It also allows us to bypass the scoping rules
+-- of SQL that otherwise restrict operations like
+-- 'Opaleye.Aggregate.aggregate' and 'Opaleye.Binary.union' to 'Select's
+-- rather than 'SelectArr's.
+lateral :: (i -> Select a) -> SelectArr i a
+lateral f = QueryArr qa
+  where
+    qa (i, primQueryL, tag) = (a, primQueryJoin, tag')
+      where
+        (a, primQueryR, tag') = runSimpleQueryArr (f i) ((), tag)
+        primQueryJoin = PQ.Product ((PQ.NonLateral, primQueryL)
+                                    :| [(PQ.Lateral, primQueryR)])
+                                   []
+
+viaLateral :: SelectArr i a -> i -> Select a
+viaLateral s i = s <<< pure i
+
+bind :: SelectArr i a -> (a -> SelectArr i b) -> SelectArr i b
+bind s f = proc i -> do
+  a <- s -< i
+  b <- lateral (\(a, i) -> viaLateral (f a) i) -< (a, i)
+  returnA -< b
+
+arrowApply :: SelectArr (SelectArr i a, i) a
+arrowApply = lateral (\(f, i) -> viaLateral f i)
 
 instance C.Category QueryArr where
   id = QueryArr id
