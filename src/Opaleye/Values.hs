e88101c618c80113b0b1b7f0ab7cac90a617f22c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-08-12 23:35:28 +0100

    Replace values with valuesSafe

--- src/Opaleye/Values.hs
+++ src/Opaleye/Values.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Opaleye.Values(
-  valuesSafe,
+  values,
   -- * Explicit versions
-  valuesSafeExplicit,
+  valuesExplicit,
   -- * Adaptors
   V.Valuesspec,
   V.ValuesspecSafe,
   V.valuesspecField,
   -- * Deprecated versions
-  values,
+  valuesSafe,
+  valuesSafeExplicit,
   valuesUnsafe,
-  valuesExplicit,
   valuesUnsafeExplicit,
   ) where
 
@@ -22,59 +22,49 @@ import qualified Opaleye.Select              as S
 
 import           Data.Profunctor.Product.Default (Default, def)
 
--- | Please note that 'values' of an empty list generates incorrect
--- queries when mixed with @OUTER@\/@LEFT@\/@RIGHT JOIN@.  You should
--- use 'valuesSafe' instead.  'valuesSafe' will replace 'values' in
--- version 0.7.
-values :: (Default V.ValuesspecUnsafe fields fields,
-           Default U.Unpackspec fields fields) =>
-          [fields] -> S.Select fields
-values = valuesExplicit def def
+-- | Do not use.  Will be deprecated in 0.8.
+valuesUnsafe :: (Default V.ValuesspecUnsafe fields fields,
+                 Default U.Unpackspec fields fields) =>
+                [fields] -> S.Select fields
+valuesUnsafe = valuesUnsafeExplicit def def
 
--- | Please note that 'valuesExplict' of an empty list generates
--- incorrect queries when mixed with @OUTER@\/@LEFT@\/@RIGHT JOIN@.
--- You should use 'valuesSafeExplicit' instead.  'valuesSafeExplicit'
--- will replace 'valuesExplicit' in version 0.7.
-valuesExplicit :: U.Unpackspec fields fields'
-               -> V.ValuesspecUnsafe fields fields'
-               -> [fields] -> S.Select fields'
-valuesExplicit unpack valuesspec fields =
+-- | Do not use.  Will be deprecated in 0.8.
+valuesUnsafeExplicit :: U.Unpackspec fields fields'
+                     -> V.ValuesspecUnsafe fields fields'
+                     -> [fields] -> S.Select fields'
+valuesUnsafeExplicit unpack valuesspec fields =
   Q.productQueryArr (V.valuesU unpack valuesspec fields)
 
--- | 'valuesSafe' implements Postgres's @VALUES@ construct and allows you
+-- | 'values' implements Postgres's @VALUES@ construct and allows you
 -- to create a @SELECT@ that consists of the given rows.
 --
 -- Example type specialization:
 --
 -- @
--- valuesSafe :: [(Field a, Field b)] -> Select (Field a, Field b)
+-- values :: [(Field a, Field b)] -> Select (Field a, Field b)
 -- @
 --
 -- Assuming the @makeAdaptorAndInstance@ splice has been run for the
 -- product type @Foo@:
 --
 -- @
--- valuesSafe :: [Foo (Field a) (Field b) (Field c)] -> S.Select (Foo (Field a) (Field b) (Field c))
+-- values :: [Foo (Field a) (Field b) (Field c)] -> S.Select (Foo (Field a) (Field b) (Field c))
 -- @
-valuesSafe :: Default V.Valuesspec fields fields =>
-               [fields] -> S.Select fields
-valuesSafe = valuesSafeExplicit def
+values :: Default V.Valuesspec fields fields
+       => [fields] -> S.Select fields
+values = valuesExplicit def
 
-valuesSafeExplicit :: V.Valuesspec fields fields'
-                   -> [fields] -> S.Select fields'
-valuesSafeExplicit valuesspec fields =
+valuesExplicit :: V.Valuesspec fields fields'
+               -> [fields] -> S.Select fields'
+valuesExplicit valuesspec fields =
   Q.productQueryArr (V.valuesUSafe valuesspec fields)
 
--- | Forward-compatible version of unsafe 'values' that will not be
--- deprecated in 0.7, but in 0.8.
-valuesUnsafe :: (Default V.ValuesspecUnsafe fields fields,
-                 Default U.Unpackspec fields fields) =>
-                [fields] -> S.Select fields
-valuesUnsafe = values
+-- | Use 'values' instead.  Will be deprecated in 0.8.
+valuesSafe :: Default V.Valuesspec fields fields
+           => [fields] -> S.Select fields
+valuesSafe = values
 
--- | Forward compatible version of unsafe 'valuesExplicit' that will
--- not be deprecated in 0.7, but in 0.8.
-valuesUnsafeExplicit :: U.Unpackspec fields fields'
-                     -> V.ValuesspecUnsafe fields fields'
-                     -> [fields] -> S.Select fields'
-valuesUnsafeExplicit = valuesExplicit
+-- | Use 'valuesExplicit' instead.  Will be deprecated in 0.8.
+valuesSafeExplicit :: V.Valuesspec fields fields'
+                   -> [fields] -> S.Select fields'
+valuesSafeExplicit = valuesExplicit
