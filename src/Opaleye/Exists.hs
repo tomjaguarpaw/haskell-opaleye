294a5fe58b6ebde02b98b52ab0917add884285b0
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-06-19 13:44:03 +0100

    Use productQueryArr instead of runSimpleQueryArr

--- src/Opaleye/Exists.hs
+++ src/Opaleye/Exists.hs
@@ -2,7 +2,7 @@ module Opaleye.Exists (exists) where
 
 import           Opaleye.Field (Field)
 import           Opaleye.Internal.Column (Column (Column))
-import           Opaleye.Internal.QueryArr (runSimpleQueryArr, simpleQueryArr)
+import           Opaleye.Internal.QueryArr (runSimpleQueryArr, productQueryArr)
 import           Opaleye.Internal.PackMap (run, extractAttr)
 import           Opaleye.Internal.PrimQuery (PrimQuery' (Exists))
 import           Opaleye.Internal.Tag (next)
@@ -13,7 +13,7 @@ import           Opaleye.SqlTypes (SqlBool)
 --
 -- This operation is equivalent to Postgres's @EXISTS@ operator.
 exists :: Select a -> Select (Field SqlBool)
-exists q = simpleQueryArr (f . runSimpleQueryArr q)
+exists q = productQueryArr (f . runSimpleQueryArr q)
   where
     f (_, query, tag) = (Column result, Exists binding query, tag')
       where
