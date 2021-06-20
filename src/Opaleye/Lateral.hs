2b31fffeee12b23f22d4cc871f97e2f22f6e87e8
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-06-20 15:49:43 +0100

    Explain in Haddock when (explicit) lateral functions are needed

--- src/Opaleye/Lateral.hs
+++ src/Opaleye/Lateral.hs
@@ -1,3 +1,8 @@
+-- | You will only need this module if you are using the arrow
+-- ('Opaleye.Select.SelectArr') interface to Opaleye.  It is not
+-- needed when working only with 'Opaleye.Select.Select's and using
+-- the monadic (@do@ notation) interface.
+
 module Opaleye.Lateral
   ( lateral
   , viaLateral
