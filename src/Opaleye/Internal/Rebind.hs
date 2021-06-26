5b682b3784628673100b36ad0dc69782e357ff3d
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-06-26 18:41:28 +0100

    Add Rebind module

---
+++ src/Opaleye/Internal/Rebind.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Opaleye.Internal.Rebind where
+
+import Data.Profunctor.Product.Default (Default, def)
+import Opaleye.Internal.Unpackspec (Unpackspec, runUnpackspec)
+import Opaleye.Internal.QueryArr (SelectArr(QueryArr))
+import qualified Opaleye.Internal.PackMap as PM
+import qualified Opaleye.Internal.PrimQuery as PQ
+import qualified Opaleye.Internal.Tag as Tag
+
+rebind :: Default Unpackspec a a => SelectArr a a
+rebind = QueryArr (\(a, tag) ->
+                     let (b, bindings) = PM.run (runUnpackspec def (PM.extractAttr "rebind" tag) a)
+                     in (b, \_ -> PQ.Rebind True bindings, Tag.next tag))
