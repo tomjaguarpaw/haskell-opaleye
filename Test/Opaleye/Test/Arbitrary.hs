61fcda0ecad37cb92bf985af553e327df273bc24
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-01-30 19:09:07 +0000

    Add exists to Arbitrary instances

--- Test/Opaleye/Test/Arbitrary.hs
+++ Test/Opaleye/Test/Arbitrary.hs
@@ -13,6 +13,7 @@ import           Prelude hiding (compare, (.), id)
 import           Opaleye.Test.Fields
 
 import qualified Opaleye as O
+import qualified Opaleye.Exists as OE
 import qualified Opaleye.Join as OJ
 
 import           Control.Applicative (pure, (<$>), (<*>), liftA2)
@@ -497,6 +498,10 @@ genSelectMapper =
         pure $ \select -> do
           O.label' thisLabel
           select
+    , do
+        pure $ \select -> do
+          exists <- OE.exists select
+          pure (Choices [Left (CBool exists)])
     ]
 
 genSelectMapper2 :: [TQ.Gen (O.Select Fields -> O.Select Fields
