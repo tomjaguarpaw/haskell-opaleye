1736eb299436cad707f86f7abe6bef2499b7c6e4
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-01-30 16:13:44 +0000

    Inline bindings

--- src/Opaleye/Label.hs
+++ src/Opaleye/Label.hs
@@ -8,6 +8,4 @@ import qualified Opaleye.Select            as S
 
 -- | Add a commented label to the generated SQL.
 label :: String -> S.SelectArr a b -> S.SelectArr a b
-label l s = Q.QueryArr ((\(b, pqf, t) -> (b, \lat -> f . pqf lat, t)) . Q.runQueryArr sa)
-  where f = PQ.Label l
-        sa = s
+label l s = Q.QueryArr ((\(b, pqf, t) -> (b, \lat -> PQ.Label l . pqf lat, t)) . Q.runQueryArr s)
