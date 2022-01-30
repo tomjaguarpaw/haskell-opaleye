cf523d87609aaca2852436a3c66a8e2b47945e6a
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-01-30 22:35:03 +0000

    Use productQueryArr' in Exists

--- src/Opaleye/Exists.hs
+++ src/Opaleye/Exists.hs
@@ -2,10 +2,10 @@ module Opaleye.Exists (exists) where
 
 import           Opaleye.Field (Field)
 import           Opaleye.Internal.Column (Field_(Column))
-import           Opaleye.Internal.QueryArr (runSimpleQueryArr, productQueryArr)
+import           Opaleye.Internal.QueryArr (productQueryArr', runSimpleQueryArr')
 import           Opaleye.Internal.PackMap (run, extractAttr)
 import           Opaleye.Internal.PrimQuery (PrimQuery' (Exists))
-import           Opaleye.Internal.Tag (next)
+import           Opaleye.Internal.Tag (fresh)
 import           Opaleye.Select (Select)
 import           Opaleye.SqlTypes (SqlBool)
 
@@ -13,9 +13,8 @@ import           Opaleye.SqlTypes (SqlBool)
 --
 -- This operation is equivalent to Postgres's @EXISTS@ operator.
 exists :: Select a -> Select (Field SqlBool)
-exists q = productQueryArr (f . runSimpleQueryArr q)
-  where
-    f (_, query, tag) = (Column result, Exists binding query, tag')
-      where
-        (result, [(binding, ())]) = run (extractAttr "exists" tag ())
-        tag' = next tag
+exists q = productQueryArr' $ \() -> do
+  (_, query) <- runSimpleQueryArr' q ()
+  tag <- fresh
+  let (result, [(binding, ())]) = run (extractAttr "exists" tag ())
+  pure (Column result, Exists binding query)
