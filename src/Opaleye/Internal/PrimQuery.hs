3b55d22197f15dfde424c4d03d3b0a2c2bf56e20
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-05-06 15:33:34 +0100

    Introduce Aggr synonym

--- src/Opaleye/Internal/PrimQuery.hs
+++ src/Opaleye/Internal/PrimQuery.hs
@@ -128,9 +128,7 @@ data PrimQuery' a = Unit
                   | Product   (NEL.NonEmpty (Lateral, PrimQuery' a)) [HPQ.PrimExpr]
                   -- | The subqueries to take the product of and the
                   --   restrictions to apply
-                  | Aggregate (Bindings (Maybe (HPQ.AggrOp,
-                                                [HPQ.OrderExpr],
-                                                HPQ.AggrDistinct),
+                  | Aggregate (Bindings (HPQ.Aggr,
                                           HPQ.Symbol))
                               (PrimQuery' a)
                   | Window (Bindings (HPQ.WndwOp, HPQ.Partition)) (PrimQuery' a)
@@ -176,8 +174,7 @@ data PrimQueryFoldP a p p' = PrimQueryFold
   , empty             :: a -> p'
   , baseTable         :: TableIdentifier -> Bindings HPQ.PrimExpr -> p'
   , product           :: NEL.NonEmpty (Lateral, p) -> [HPQ.PrimExpr] -> p'
-  , aggregate         :: Bindings (Maybe
-                             (HPQ.AggrOp, [HPQ.OrderExpr], HPQ.AggrDistinct),
+  , aggregate         :: Bindings (HPQ.Aggr,
                                    HPQ.Symbol)
                       -> p
                       -> p'
