7fbcd6a5315f9f17e344ceafbb7617c68f4cec2c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-05-06 15:05:25 +0100

    Pull out variable

--- src/Opaleye/Internal/Aggregate.hs
+++ src/Opaleye/Internal/Aggregate.hs
@@ -78,7 +78,9 @@ makeAggr = makeAggr' . Just
 
 orderAggregate :: O.Order a -> Aggregator a b -> Aggregator a b
 orderAggregate o (Aggregator (PM.PackMap pm)) = Aggregator (PM.PackMap
-  (\f c -> pm (f . P.first' (fmap ((\f' (a,b,c') -> (a,f' b,c')) (const (O.orderExprs c o))))) c))
+  (\f c -> pm (f . P.first' (fmap (g (const (O.orderExprs c o))))) c))
+  where
+    g = \f' (a,b,c') -> (a,f' b,c')
 
 runAggregator
   :: Applicative f
